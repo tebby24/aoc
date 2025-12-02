ffc57dc46e161816c27d3914cfc03a14c2b1bdfe
Teddy Gonyea <enterted@gmail.com>
2025-12-02 17:50:41 -0500

    day 2 part 2 done!!

--- 2025/2/main.cpp
+++ 2025/2/main.cpp
@@ -2,32 +2,59 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <set>
 
-long long sum_inval_ids(long long start, long long end) {
-    long long sum = 0;
 
+std::string repeat_string(std::string& str, int n) {
+    std::string res = "";
+    for (int i = 0; i < n; i++) {
+        res += str;
+    }
+    return res;
+}
+
+std::vector<long long> find_invalid_ids(long long start, long long end, int repeat_count) {
+    std::vector<long long> ids = {};
     std::string start_str = std::to_string(start);
-    std::string half;
-
-    if (start_str.length() == 1) { half = "1"; }
+    std::string sub;
+    int repeat_length;
+    
+    if (start_str.length() == 1) {
+        repeat_count = 2;
+        sub = "1";
+    }
     else {
-        int mid = start_str.length() / 2;
-        half = start_str.substr(0, mid);
+        if (repeat_count > start_str.length()) {
+            sub = "1";
+        }
+        else {
+            repeat_length = start_str.length() / repeat_count;
+            sub = start_str.substr(0, repeat_length);
+        }
     }
 
-    while (std::stoll(half+half) < start) {
-        half = std::to_string(std::stoll(half)+1);
+    while (std::stoll(repeat_string(sub, repeat_count)) < start) {
+        sub = std::to_string(std::stoll(sub) + 1);
     }
-    while (std::stoll(half+half) <= end) {
-        sum += std::stoll(half+half);
-        half = std::to_string(std::stoll(half)+1);
+
+    while (std::stoll(repeat_string(sub, repeat_count)) <= end) {
+        ids.push_back(std::stoll(repeat_string(sub, repeat_count)));
+        sub = std::to_string(std::stoll(sub) + 1);
     }
-    return sum;
+    
+    return ids;
 }
 
 
 int main(int argc, char* argv[]) {    
-    std::ifstream fin("input.txt");
+    if (argc != 2) {
+        std::cerr << "useage: " << argv[0] << " <filepath>" << std::endl;
+        return EXIT_FAILURE;
+    }    
+
+    std::string filepath = argv[1];
+    std::ifstream fin(filepath);
 
     std::string line;
     std::getline(fin, line);
@@ -35,16 +62,37 @@ int main(int argc, char* argv[]) {
     std::stringstream ss(line);
     std::string token;
 
-    long long sum = 0;
+    std::set<long long> silver_ids = {};
+    std::set<long long> gold_ids = {};
     while(std::getline(ss, token, ',')){
         auto pos = token.find('-');
         long long start = std::stoll(token.substr(0, pos));
         long long end = std::stoll(token.substr(pos+1));
 
-        sum += sum_inval_ids(start, end);
+        int start_digits = std::to_string(start).length();
+        int end_digits = std::to_string(end).length();
+        for (int i = 2; i <= end_digits; i++) {
+            for (long long& id : find_invalid_ids(start, end, i)) {
+                if (i == 2) {
+                    silver_ids.insert(id);
+                }
+                gold_ids.insert(id);
+            }
+        }
     }
 
-    std::cout << "invalid id sum: " << sum << std::endl;
+    long long silver_sum = 0;
+    for (long long id : silver_ids) {
+        silver_sum += id;
+    }
+    long long gold_sum = 0;
+    for (long long id : gold_ids) {
+        gold_sum += id;
+    }
+
+    std::cout << "silver ids: " << silver_sum << std::endl;
+    std::cout << "gold ids: " << gold_sum << std::endl;
+
 
     return 0;
 }
